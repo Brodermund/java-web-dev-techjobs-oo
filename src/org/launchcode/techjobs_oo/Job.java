556cd5eefa23e611eeb752e1d2b6b1fff78f6348
Brodermund <Rodermundbobby@yahoo.com>
2021-05-11 16:24:50 -0500

    Work -> Home

--- src/org/launchcode/techjobs_oo/Job.java
+++ src/org/launchcode/techjobs_oo/Job.java
@@ -1,6 +1,6 @@
 package org.launchcode.techjobs_oo;
 
-import java.util.Objects;
+import java.util.*;
 
 public class Job {
 
@@ -25,6 +25,84 @@ public class Job {
         this.positionType = positionType;
         this.coreCompetency = coreCompetency;
     }
+    public String toString(Job job){
+        HashMap<String, String> jobMap = new HashMap<>();
+        ArrayList<String> endList = new ArrayList<>();
+        String id = String.valueOf(getId());
+        jobMap.put("Core Competency:",job.getCoreCompetency().getValue());
+        jobMap .put("Position Type:",job.getPositionType().getValue());
+        jobMap.put("Location:",job.getLocation().getValue());
+        jobMap.put("Employer:",job.getEmployer().getValue());
+        jobMap.put("Name:", job.getName());
+        jobMap.put("ID:",id);
+
+
+//        String name = ("Name: _" + job.getName() + "_");
+//        String employer =("Employer: _" + job.getEmployer().getValue() + "_");
+//        String location =("Location: _" + job.getLocation().getValue() + "_");
+//        String positionType =("Position Type: _" + job.getPositionType().getValue() + "_");
+//        String coreCompetency =("Core Competency: _" + job.getCoreCompetency().getValue() + "_");
+        for (Map.Entry<String, String> entry : jobMap.entrySet()) {
+            String value = entry.getValue();
+            String key = entry.getKey();
+            if (value.length() == 0 || value.isBlank()){
+                endList.add(key + "_Data not available_");
+            }
+            else{
+                endList.add(key + "_"+value+"_");
+
+
+            }
+            endList.add(System.lineSeparator());
+
+        }
+        String fin = String.join("",endList);
+
+
+//        String l = System.lineSeparator();
+//        String jobString = ( id + l + name + l + employer + l + location + l + positionType + l + coreCompetency);
+//        if (job.getName().length() == 0 || job.getName().isBlank()){
+//            joblist.put("Name:", "_Data not available_");
+//        }
+//        else {
+//            joblist.put("Name:","_" + job.getName() + "_");
+//        }
+//        if (job.getEmployer().getValue().length() == 0 || job.getEmployer().getValue().isBlank()){
+//            String employer =("Employer: _Data not available_");
+//        }
+//        else {
+//            String employer =("Employer: _" + job.getEmployer().getValue() + "_");
+//        }
+//        if (job.getLocation().getValue().length() == 0 || job.getLocation().getValue().isBlank()){
+//            String location =("Location: _Data not available_");
+//        }
+//        else {
+//            String location =("Location: _" + job.getLocation().getValue() + "_");
+//
+//        }
+//        if (job.getPositionType().getValue().length() == 0 || job.getPositionType().getValue().isBlank()){
+//            String positionType =("Position Type: _Data not available_");
+//        }
+//        else {
+//            String positionType =("Position Type: _" + job.getPositionType().getValue() + "_");
+//
+//        }
+//        if (job.getCoreCompetency().getValue().length() == 0 || job.getCoreCompetency().getValue().isBlank()){
+//            String coreCompetency =("Core Competency: _Data not available_");
+//        }else {
+//            String coreCompetency =("Core Competency: _" + job.getCoreCompetency().getValue() + "_");
+//
+//        }
+//        String jobString = ( id + l + name + l + employer + l + location + l + positionType + l + coreCompetency);
+
+
+
+
+//        r
+        return fin;
+
+    }
+
 
     @Override
     public boolean equals(Object o) {
--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -1,10 +1,9 @@
 package org.launchcode.techjobs_oo.Tests;
 
 import org.junit.Test;
-import org.launchcode.techjobs_oo.Job;
+import org.launchcode.techjobs_oo.*;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 public class JobTest {
     @Test
@@ -15,5 +14,53 @@ public class JobTest {
         int id2 = job2.getId();
         assertFalse((id1 == id2));
     }
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        Job testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertEquals(testJob.getName(), "Product tester");
+        assertEquals(testJob.getEmployer().getValue(),"ACME");
+        assertEquals(testJob.getLocation().getValue(),"Desert");
+        assertEquals(testJob.getPositionType().getValue(),"Quality control");
+        assertEquals(testJob.getCoreCompetency().getValue(),"Persistence");
+
+    }
+    @Test
+    public void testJobsForEquality(){
+        Job testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job testJob2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        int id1 = testJob.getId();
+        int id2 = testJob2.getId();
+        assertEquals(testJob.getName(), testJob2.getName());
+        assertEquals(testJob.getEmployer().getValue(),testJob2.getEmployer().getValue());
+        assertEquals(testJob.getLocation().getValue(),testJob2.getLocation().getValue());
+        assertEquals(testJob.getPositionType().getValue(),testJob2.getPositionType().getValue());
+        assertEquals(testJob.getCoreCompetency().getValue(),testJob2.getCoreCompetency().getValue());
+        assertFalse((id1 == id2));
+    }
+    @Test
+    public void testToString1(){
+        Job testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(testJob.toString().startsWith("_"));
+        assertTrue(testJob.toString().endsWith("_"));
+
+    }
+    @Test
+    public void testToString2(){
+        Job testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(testJob.toString().startsWith("_"));
+        assertTrue(testJob.toString().endsWith("_"));
+
+    }
+    @Test
+    public void testToString5(){
+        Job testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job testJob2 = new Job();
+        System.out.println(testJob.toString(testJob2));
+
+    }
+
+
+
+
 }
 
